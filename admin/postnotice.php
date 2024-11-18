bbbcfbfbea1a0517c0fcd64516a790f6f29a3c9f
Santhosh S <santhosh.senthil552003@gmail.com>
2024-11-18 17:57:06 +0530

    notice feature UI corrected

--- admin/postnotice.php
+++ admin/postnotice.php
@@ -1,127 +1,54 @@
 <?php
-
-//To Handle Session Variables on This Page
-
-
 session_start();
-
-
-//Including Database Connection From db.php file to avoid rewriting in all files
 require_once("../db.php");
 
-
-
 if (isset($_POST['submit'])) {
-
-    // they take values using name attribute 
     $subject = $_POST['subject'];
     $notice = $_POST['input'];
     $audience = $_POST['audience'];
-
     $file = "";
     $uploadOk = true;
 
-		//Folder where you want to save your resume. THIS FOLDER MUST BE CREATED BEFORE TRYING
-		$folder_dir = "../uploads/notice/";
+    // Folder for file uploads
+    $folder_dir = "../uploads/notice/";
 
-		//Getting Basename of file. So if your file location is Documents/New Folder/myResume.pdf then base name will return myResume.pdf
-		$base = basename($_FILES['notice']['name']);
+    if (isset($_FILES['notice']) && file_exists($_FILES['notice']['tmp_name'])) {
+        $base = basename($_FILES['notice']['name']);
+        $docFileType = pathinfo($base, PATHINFO_EXTENSION);
+        $file = uniqid() . "." . $docFileType;
+        $filename = $folder_dir . $file;
 
-		//This will get us extension of your file. So myResume.pdf will return pdf. If it was resume.doc then this will return doc.
-		$docFileType = pathinfo($base, PATHINFO_EXTENSION);
-
-		//Setting a random non repeatable file name. Uniqid will create a unique name based on current timestamp. We are using this because no two files can be of same name as it will overwrite.
-		$file = uniqid() . "." . $docFileType;
-
-		//This is where your files will be saved so in this case it will be uploads/resume/newfilename
-		$filename = $folder_dir . $file;
-
-		//We check if file is saved to our temp location or not.
-		if (file_exists($_FILES['notice']['tmp_name'])) {
-
-			//Next we need to check if file type is of our allowed extention or not. I have only allowed pdf. You can allow doc, jpg etc. 
-			if ($docFileType == "pdf") {
-
-				//Next we need to check file size with our limit size. I have set the limit size to 5MB. Note if you set higher than 2MB then you must change your php.ini configuration and change upload_max_filesize and restart your server
-				if ($_FILES['notice']['size'] < 500000) { // File size is less than 5MB
-
-					//If all above condition are met then copy file from server temp location to uploads folder.
-					move_uploaded_file($_FILES["notice"]["tmp_name"], $filename);
-				} else {
-					//Size Error
-					$_SESSION['uploadError'] = "Wrong Size. Max Size Allowed : 5MB";
-					$uploadOk = false;
-				}
-			} else {
-				//Format Error
-				$_SESSION['uploadError'] = "Wrong Format. Only PDF Allowed";
-				$uploadOk = false;
-			}
-		} else {
-			//File not copied to temp location error.
-			$_SESSION['uploadError'] = "Something Went Wrong. File Not Uploaded. Try Again.";
-			$uploadOk = false;
-		}
-
-        if ($uploadOk) {
-            $hash = md5(uniqid());
-            $sql = "INSERT INTO notice(subject, notice, audience, document, hash, `date`) VALUES ('$subject', '$notice', '$audience', '$file', '$hash', now())";
-    
-            if ($conn->query($sql) === TRUE) {
-                include 'sendmail.php';
-                header("Location: postnotice.php");
-                exit();
+        if ($docFileType == "pdf") {
+            if ($_FILES['notice']['size'] <= 5000000) { // File size is less than 5MB
+                if (!move_uploaded_file($_FILES["notice"]["tmp_name"], $filename)) {
+                    $_SESSION['uploadError'] = "File upload failed. Please try again.";
+                    $uploadOk = false;
+                }
             } else {
-                echo "Error: " . $conn->error;
+                $_SESSION['uploadError'] = "Wrong Size. Max Size Allowed: 5MB";
+                $uploadOk = false;
             }
-        } 
+        } else {
+            $_SESSION['uploadError'] = "Wrong Format. Only PDF Allowed";
+            $uploadOk = false;
+        }
+    }
+
+    if ($uploadOk) {
+        $hash = md5(uniqid());
+        $stmt = $conn->prepare("INSERT INTO notice (subject, notice, audience, document, hash, `date`) VALUES (?, ?, ?, ?, ?, NOW())");
+        $stmt->bind_param("sssss", $subject, $notice, $audience, $file, $hash);
+
+        if ($stmt->execute()) {
+            include 'sendmail.php';
+            header("Location: postnotice.php");
+            exit();
+        } else {
+            echo "Error: " . $conn->error;
+        }
+        $stmt->close();
+    }
 }
-		
-		
-    // // Check if a file is uploaded
-    // if (isset($_FILES['resume']['name']) && !empty($_FILES['resume']['name'])) {
-    // //Folder where you want to save your resume. THIS FOLDER MUST BE CREATED BEFORE TRYING
-    // $folder_dir = "../uploads/resume/";
-
-    // //Getting Basename of file. So if your file location is Documents/New Folder/myResume.pdf then base name will return myResume.pdf
-    // $base = basename($_FILES['resume']['name']);
-
-    // //This will get us extension of your file. So myResume.pdf will return pdf. If it was resume.doc then this will return doc.
-    // $resumeFileType = pathinfo($base, PATHINFO_EXTENSION);
-
-    // //Setting a random non repeatable file name. Uniqid will create a unique name based on current timestamp. We are using this because no two files can be of same name as it will overwrite.
-    // $file = uniqid() . "." . $resumeFileType;
-
-    // //This is where your files will be saved so in this case it will be uploads/resume/newfilename
-    // $filename = $folder_dir . $file;
-
-    // //We check if file is saved to our temp location or not.
-    // if (file_exists($_FILES['resume']['tmp_name'])) {
-    //     move_uploaded_file(
-    //         $_FILES["resume"]["tmp_name"],
-    //         $filename
-    //     );
-    // }else
-    //     // File failed to move
-    //     echo "Error: Failed to upload the file.";
-    // }
-    // else 
-    // echo "Error: Temporary file not found.";
-
-//     $hash = md5(uniqid());
-
-
-//     $sql = "INSERT INTO notice(subject,notice,audience,resume, hash,`date`) VALUES ('$subject','$notice','$audience','$file', '$hash',now())";
-
-//     if ($conn->query($sql) === TRUE) {
-//         include 'sendmail.php';
-//         header("Location: postnotice.php");
-//         exit();
-//     }
-// }
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -131,33 +58,18 @@ if (isset($_POST['submit'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Placement Portal</title>
-
-    <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
     <link rel="stylesheet" href="css/AdminLTE.min.css">
     <link rel="stylesheet" href="css/_all-skins.min.css">
-    <!-- Custom -->
     <link rel="stylesheet" href="css/custom.css">
-
-
-    <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
 <body class="hold-transition skin-green sidebar-mini">
-    <?php
-
-    include 'header.php';
-    ?>
-
-
+    <?php include 'header.php'; ?>
 
     <div class="row">
         <div class="col-xs-6 responsive">
@@ -168,143 +80,96 @@ if (isset($_POST['submit'])) {
                     New Notice Successfully added
                 </div>
 
-                <form class="centre " action="" method="POST" enctype="multipart/form-data">
+                <form class="centre" action="" method="POST" enctype="multipart/form-data">
                     <div>
-                        <h4><strong> Post a new notice</strong></h4>
+                        <h4><strong>Post a new notice</strong></h4>
                     </div>
                     <div>
-
-                        <input id="subject" placeholder="Subject" type="text" name="subject" style="margin:auto">
-
+                        <input id="subject" placeholder="Subject" type="text" name="subject" required>
                     </div>
 
                     <div id="file" class="form-group">
-                        <style>
-                            #file {
-                                margin-left: 40px;
-                                margin-top: 20px;
-                            }
-                        </style>
-                        <!-- <label style="color: red;">Attachment</label> -->
-                        <input type="file" name="notice" class="btn btn-flat btn-primary">
+                        <input type="file" class="input1" name="notice" class="btn btn-flat btn-primary">
                     </div>
 
                     <br>
                     <div class="form-group mt-3">
-                        <textarea style="top:80px " type="input" class="input" name="input" id="input" placeholder="Notice" required></textarea>
+                        <textarea class="input" name="input" id="input" placeholder="Notice" required></textarea>
                     </div>
 
                     <div class="form-group text-center option">
-                        <label>Audience </label>
-                        <select class="form-control select2 select2-hidden-accessible" style="width: 100%" tabindex="-1" aria-hidden="true" class="input" name="audience">
-
-                            <option class="option" value="All Students">All Students</option>
-                            <option class="option" value="Co-ordinators">Co-ordinators</option>
-
-
+                        <label>Audience</label>
+                        <select class="form-control select2" style="width: 100%" name="audience">
+                            <option value="All Students">All Students</option>
+                            <option value="Co-ordinators">Co-ordinators</option>
                         </select>
                     </div>
+
                     <div class="text-center">
-                        <button class="btn btn-primary btn-sm" id="submit" name="submit" type=" submit1">NOTIFY</button>
-
+                        <button class="btn btn-primary btn-sm" id="submit" name="submit" type="submit">NOTIFY</button>
                     </div><br>
-                    <div>
-                    </div>
-
-
                 </form>
-
-
+            </section>
         </div>
-        </section>
 
-
-
-        <div class="col-xs-5 responsive2 ">
-
-
-            <div class="box box-primary ">
+        <div class="col-xs-5 responsive2">
+            <div class="box box-primary">
                 <div class="box-header with-border">
                     <h3 class="box-title">Posted Notice</h3>
                 </div>
-
-                <!-- /.box-header -->
                 <div class="box-body">
                     <table id="example1" class="table table-bordered table-striped">
                         <thead>
                             <tr>
                                 <th>Subject</th>
                                 <th>Notice</th>
-
                                 <th>Audience</th>
-
                                 <th>File</th>
-
                                 <th>Date and Time</th>
                                 <th>Delete</th>
-
-
-
                             </tr>
                         </thead>
                         <tbody>
-
-
                             <?php
-
                             $sql = "SELECT * FROM notice";
-
                             $result = $conn->query($sql);
 
                             if ($result->num_rows > 0) {
-
-                                // output data of each row
                                 while ($row = $result->fetch_assoc()) {
                             ?>
-                                    <td><?php echo $row['subject']; ?></td>
-                                    <td><?php echo $row['notice']; ?></td>
-                                    <td><?php echo $row['audience']; ?></td>
-                                    <?php if ($row['notice'] != '') { ?>
-                                        <td><a href="../uploads/notice/<?php echo $row['document']; ?>" download="<?php echo 'Notice'; ?>"><i class="fa fa-file"></i></a></td>
-                                    <?php } else { ?>
-                                        <td>No Document Uploaded</td>
-                                    <?php } ?>
-                                    <td><?php echo $row['date']; ?></td>
-
-                                    <td><a id="delete" href="deletenotice.php?id=<?php echo $row['id']; ?>"><i class="fa fa-trash"></i></a></td>
-                                    </tr><?php
-
-                                        }
-                                    }
-
-                                            ?>
-
-
+                                    <tr>
+                                        <td><?php echo $row['subject']; ?></td>
+                                        <td><?php echo $row['notice']; ?></td>
+                                        <td><?php echo $row['audience']; ?></td>
+                                        <td>
+                                            <?php if ($row['document'] != '') { ?>
+                                                <a href="../uploads/notice/<?php echo $row['document']; ?>" download="Notice">
+                                                    <i class="fa fa-file"></i>
+                                                </a>
+                                            <?php } else { ?>
+                                                No Document Uploaded
+                                            <?php } ?>
+                                        </td>
+                                        <td><?php echo $row['date']; ?></td>
+                                        <td><a href="deletenotice.php?id=<?php echo $row['id']; ?>"><i class="fa fa-trash"></i></a></td>
+                                    </tr>
+                            <?php
+                                }
+                            }
+                            ?>
                         </tbody>
-                        <tfoot>
-
-                        </tfoot>
                     </table>
                 </div>
-                <!-- /.box-body -->
             </div>
-
         </div>
-
     </div>
 
-
-    <footer class="main-footer" style="margin:auto;margin-bottom: 0px; padding:15px;
-  width: 100%;
-  height: 50px; position:absolute; background-color:#1f0a0a; color:white">
+    <footer class="main-footer" style="margin:auto; padding:15px; width:100%; height:50px; position:absolute; background-color:#1f0a0a; color:white">
         <div class="text-center">
-            <strong>Copyright &copy; 2025 Placement Portal</strong> All rights
-            reserved.
+            <strong>Copyright &copy; 2025 Placement Portal</strong> All rights reserved.
         </div>
     </footer>
-
 </body>
-
 </html>
 
 
@@ -373,22 +238,20 @@ if (isset($_POST['submit'])) {
         .input1 {
             width: auto;
             height: auto;
+            margin: 14px 242px;
         }
 
         .centre {
-
             height: 105%;
             width: 105%;
-            margin-left: 100px;
-
+            margin-left: 367px;
         }
 
         .responsive2 {
-            margin: auto;
             display: block;
             height: 80%;
             width: 80%;
-            margin: auto;
+            margin-left: 167px;
         }
 
         #subject {
